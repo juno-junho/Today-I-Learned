1d1258b2c7924102de8640be7baa1a54335c1335
juno-junho <ssmm0205@naver.com>
2023-06-19 17:44:51 +0900

    스프링부트 Part1 project setup

---
+++ 프로그래머스 강의 실습/ktd-spring-order/src/main/java/com/prgrms/ktd/KtdSpringOrderApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.ktd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtdSpringOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtdSpringOrderApplication.class, args);
+    }
+
+}
---
+++ 프로그래머스 강의 실습/ktd-spring-order/src/test/java/com/prgrms/ktd/KtdSpringOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.ktd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtdSpringOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
