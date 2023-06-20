456da17b14708e53d0ef512f70b5874e734d0b7d
juno-junho <ssmm0205@naver.com>
2023-06-20 11:50:11 +0900

    spring part1(2) - 스프링을 사용한 자바 기반 설정 메타데이터를 통해 ApplicationContext로의 변경

--- 프로그래머스 강의 실습/ktd-spring-order/src/main/java/com/prgrms/ktd/AppConfiguration.java
+++ 프로그래머스 강의 실습/ktd-spring-order/src/main/java/com/prgrms/ktd/AppConfiguration.java
@@ -1,5 +1,8 @@
 package com.prgrms.ktd;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,35 +10,33 @@ import java.util.UUID;
  * VoucherService, VoucherRepository
  * OrderService, OrderRepository 생성에 대한 책임을 갖게 됨
  * 각각의 repository들과 service간의 wiring, 의존관계 맺는 것을 담당
-
+ * <p>
  * 주문에 대한 전반적인 도메인 객체에 대한 생성 책임을 지고 있다
-
+ * <p>
  * ioc container의 역할
  * -> 개별 객체들의 의존관계 설정 및 객체 생성 및 파괴 관장
  */
-public class OrderContext {
+@Configuration  // configuration metadata라고 알려주는 것. bean 정의한 도면이다!
+public class AppConfiguration {
+
+    @Bean
     public VoucherRepository voucherRepository() {
-        return new VoucherRepository() {
-            @Override
-            public Optional<Voucher> findById(UUID voucherId) {
-                return Optional.empty();
-            }
-        };
+        return voucherId -> Optional.empty();
     }
 
+    @Bean
     public OrderRepository orderRepository() {
-        return new OrderRepository() {
-            @Override
-            public void insert(Order order) {
+        return order -> {
 
-            }
         };
     }
 
+    @Bean
     public VoucherService voucherService() {
         return new VoucherService(voucherRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderService(voucherService(), orderRepository());
     }
--- 프로그래머스 강의 실습/ktd-spring-order/src/main/java/com/prgrms/ktd/OrderTester.java
+++ 프로그래머스 강의 실습/ktd-spring-order/src/main/java/com/prgrms/ktd/OrderTester.java
@@ -1,5 +1,6 @@
 package com.prgrms.ktd;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
 import java.text.MessageFormat;
@@ -8,9 +9,12 @@ import java.util.UUID;
 
 public class OrderTester {
     public static void main(String[] args) {
+        var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
+
         var customerId = UUID.randomUUID();
-        var orderContext = new OrderContext();
-        var orderService = orderContext.orderService();
+//        var orderService = orderContext.orderService();
+        var orderService = applicationContext.getBean(OrderService.class);
         var orderItems = new ArrayList<OrderItem>() {{
             add(new OrderItem(UUID.randomUUID(), 100L, 1L));
         }};
