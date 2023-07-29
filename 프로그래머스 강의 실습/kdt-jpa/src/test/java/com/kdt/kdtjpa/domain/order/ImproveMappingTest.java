38433e148258d6822b67f16e3317dac5407acfdb
junho <ssmm0205@naver.com>
2023-07-30 03:52:14 +0900

    상속관계 매핑 - mappedSuperClass 사용 연습

--- 프로그래머스 강의 실습/kdt-jpa/src/test/java/com/kdt/kdtjpa/domain/order/ImproveMappingTest.java
+++ 프로그래머스 강의 실습/kdt-jpa/src/test/java/com/kdt/kdtjpa/domain/order/ImproveMappingTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static com.kdt.kdtjpa.domain.order.OrderStatus.OPENED;
+
 @Slf4j
 @SpringBootTest
 class ImproveMappingTest {
@@ -28,6 +33,26 @@ class ImproveMappingTest {
         food.setChef("백종원");
 
         entityManager.persist(food);
+
+        transaction.commit();
+    }
+
+    @Test
+    void MappedSuperClsss_Test() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Order order = new Order();
+        order.setUuid(UUID.randomUUID().toString());
+        order.setOrderStatus(OPENED);
+        order.setMemo("부재시 전화주세요.");
+        order.setOrderDateTime(LocalDateTime.now());
+        order.setCreatedBy("Junho");
+        order.setCreatedAt(LocalDateTime.now());
+
+        entityManager.persist(order);
 
         transaction.commit();
     }
