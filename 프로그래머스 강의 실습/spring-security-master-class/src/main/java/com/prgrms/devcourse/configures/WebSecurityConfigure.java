899c4cd347f3250bd912011ab93a5cb0262a35bd
junho <ssmm0205@naver.com>
2023-08-01 02:24:16 +0900

    feat: 1일차 과제 제출

--- 프로그래머스 강의 실습/spring-security-master-class/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ 프로그래머스 강의 실습/spring-security-master-class/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,10 +25,31 @@ public class WebSecurityConfigure {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((auth) -> auth.requestMatchers("/me")
+    http
+            .passwordManagement(withDefaults())
+            .authorizeHttpRequests((auth) -> auth.requestMatchers("/me")
                     .hasAnyRole("USER", "ADMIN")
                     .anyRequest().permitAll())
-            .formLogin(withDefaults());
+            .formLogin(withDefaults())
+            .logout((logout) -> logout.logoutUrl("/logout").logoutSuccessUrl("/"))
+            .rememberMe((remember) -> remember.rememberMeParameter("remember-me")
+                    .tokenValiditySeconds(300));
+
     return http.build();
   }
-}
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+
+    UserBuilder users = User.withDefaultPasswordEncoder();
+    UserDetails admin = users.username("admin")
+            .password("admin123")
+            .roles("ADMIN")
+            .build();
+    UserDetails user = users.username("user")
+            .password("user123")
+            .roles("USER")
+            .build();
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+ }
