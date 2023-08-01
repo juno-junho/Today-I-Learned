523acc5826d136f19a4ef3dc76251cf33bf69aeb
junho <ssmm0205@naver.com>
2023-08-01 17:39:55 +0900

    feat: require channel 추가 및 user 등록 방식 추가

--- 프로그래머스 강의 실습/spring-security-master-class/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ 프로그래머스 강의 실습/spring-security-master-class/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -18,38 +18,69 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class WebSecurityConfigure {
 
-  @Bean
-  public WebSecurityCustomizer webSecurityCustomizer() {
-    return (web) -> web.ignoring().requestMatchers("/assets/**");// 여기에 적용하면 spring security filter chain을 태우지 않겠다
-  }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/assets/**");// 여기에 적용하면 spring security filter chain을 태우지 않겠다
+    }
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-            .passwordManagement(withDefaults())
-            .authorizeHttpRequests((auth) -> auth.requestMatchers("/me")
-                    .hasAnyRole("USER", "ADMIN")
-                    .anyRequest().permitAll())
-            .formLogin(withDefaults())
-            .logout((logout) -> logout.logoutUrl("/logout").logoutSuccessUrl("/"))
-            .rememberMe((remember) -> remember.rememberMeParameter("remember-me")
-                    .tokenValiditySeconds(300));
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       return http
+              .authorizeHttpRequests((auth) -> auth
+                      .requestMatchers("/me")
+                      .hasAnyRole("USER", "ADMIN")
+                      .anyRequest()
+                      .permitAll())
+              .formLogin(withDefaults())
+              .logout((logout) -> logout
+                      .logoutUrl("/logout")
+                      .logoutSuccessUrl("/")
+                      .invalidateHttpSession(true)
+                      .clearAuthentication(true))
+              .rememberMe((remember) -> remember
+                      .rememberMeParameter("remember-me")
+                      .tokenValiditySeconds(300))
+              .requiresChannel(channel -> channel
+                      .anyRequest()
+                      .requiresSecure())
+              .build();
+    }
 
-    return http.build();
-  }
+    // 방식 1
+    @Bean
+    public UserDetailsService userDetailsService() {
 
-  @Bean
-  public UserDetailsService userDetailsService() {
+        UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails admin = users.username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+        UserDetails user = users.username("user")
+                .password("user123")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 
-    UserBuilder users = User.withDefaultPasswordEncoder();
-    UserDetails admin = users.username("admin")
-            .password("admin123")
-            .roles("ADMIN")
-            .build();
-    UserDetails user = users.username("user")
+    // 방식 2
+/*  @Bean
+  public UserDetailsManager userDetailsManager() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withUsername("user")
             .password("user123")
+            .passwordEncoder(s -> passwordEncoder().encode(s))
             .roles("USER")
-            .build();
-    return new InMemoryUserDetailsManager(admin, user);
+            .build());
+    manager.createUser(User.withUsername("admin")
+            .password("admin123")
+            .passwordEncoder(s -> passwordEncoder().encode(s))
+            .roles("ADMIN")
+            .build());
+    return manager;
   }
- }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }*/
+}
