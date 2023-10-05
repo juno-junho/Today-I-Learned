1c60afb703a8cc4c62e68a1ce788ade4904eb2e3
junho <ssmm0205@naver.com>
2023-10-05 13:52:58 +0900

    ci-cd project setup

---
+++ 프로그래머스 강의 실습/learn-infra-aws/src/main/java/com/junho/learninfraaws/LearnInfraAwsApplication.java
@@ -0,0 +1,13 @@
+package com.junho.learninfraaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnInfraAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnInfraAwsApplication.class, args);
+    }
+
+}
---
+++ 프로그래머스 강의 실습/learn-infra-aws/src/test/java/com/junho/learninfraaws/LearnInfraAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junho.learninfraaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnInfraAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
